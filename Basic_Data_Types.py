b50c1832ce42c4394b5f4c62bdd12b79c12fa781
annamani <annamanik.vit@gmail.com>
2023-10-31 21:34:57 +0100

    initial commit

---
+++ Basic_Data_Types.py
@@ -0,0 +1,16 @@
+# Basic Data Types"
+## Defining a Variable"
+x = 5.593247837456069
+print(type(x))
+y=12345.8
+print(type(y))
+z=234567
+print(type(z))
+a=2j
+print(type(a))
+b="strings,examples"
+print(type(b))
+c=False
+print(type(c))
+
+   

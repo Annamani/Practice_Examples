750780826428af144c90f77d4d037f99a74e2075
annamani <annamanik.vit@gmail.com>
2023-11-20 20:28:32 +0100

    if commit

--- ifelse.py
+++ ifelse.py
@@ -1,11 +1,9 @@
 for n in range(1,35):
-    if n%15==0:
+    if n % 15==0:
         print("FizzBuzz")
+    elif n % 3==0:
+        print("Fizz")
+    elif n % 5==0:
+        print("Buzz")
     else:
-        if n%3==0:
-            print("Fizz")
-        else:
-            if n%5==0:
-                print("Buzz")
-            else:
-                print(n)
+        print(n)

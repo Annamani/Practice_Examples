4d0e8284bb71d5c47f477fd37c04ed73b0df9560
annamani <annamanik.vit@gmail.com>
2023-11-20 19:15:13 +0100

    initial commit

---
+++ datastruct.py
@@ -0,0 +1,15 @@
+#Creating a list
+myList=[1,2,3,4,5]
+print(myList)
+myList.append('i am a list')
+print(myList)
+for i in range(3):
+    print(myList[::])
+
+# Creating a Set
+mySet={6,6,4,7,8,9,10,4}
+print(mySet)
+
+# Creating a tuple
+myTuple=(11,12,13,14,15)
+print(myTuple)
---
+++ tuplesandSets.py
@@ -0,0 +1,11 @@
+# Create  aset
+mySet={1,2,3,4,5,6,1,2,0}
+print(mySet)
+
+# Create a list and print as a set
+myList=['a','b','c','a','c','d','e','f']
+print(myList)
+# conversion of a list to a set 
+# Output is as follows
+# myList=list(set(myList))
+print(set(myList))

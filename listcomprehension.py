568ee652788b100ee229432f632823299eada595
annamani <annamanik.vit@gmail.com>
2023-11-20 19:50:58 +0100

    list commit

--- listcomprehension.py
+++ listcomprehension.py
@@ -3,3 +3,8 @@ myList=[1,2,3,4,5,6]
 print(myList)
 updatedList=[2*item for item in myList]
 print(updatedList)
+
+# List comprehension with Filters
+myList2=list(range(20))
+filteredList=[item for item in myList2 if item % 2==0]
+print(filteredList)

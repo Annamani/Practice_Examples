b3d9787f6887cd5da2949f5ad12b2abca4c532e7
annamani <annamanik.vit@gmail.com>
2023-11-22 21:08:57 +0100

    func commit

--- classEx.py
+++ classEx.py
@@ -1,8 +1,21 @@
-# Instance Attributes
+# # Instance Attributes
+# class Dog:
+#     def __init__(self, name):
+#         self.name = name
+#         self.legs = 4
+    
+#     def speak(self):
+#         print(self.name + ' says: Bark!')
+
+# myDog = Dog('Rover')
+# print(myDog.name)
+# print(myDog.legs)
+
+# Static Atributes
 class Dog:
+    legs = 4
     def __init__(self, name):
         self.name = name
-        self.legs = 4
     
     def speak(self):
         print(self.name + ' says: Bark!')

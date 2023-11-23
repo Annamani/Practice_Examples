8447cadfba3bd111c61804078ee42be5db307c2e
annamani <annamanik.vit@gmail.com>
2023-11-23 21:25:11 +0100

    Exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,42 @@
+# def causeError():
+#     try:
+#         return 1/0
+#     except Exception as e:
+#         print(type(e))
+#     finally:
+#         print("This block always executed")
+# causeError()
+
+# def causeError():
+#     try:
+#         # return 1 + 'a'
+#         return 1/0
+
+#     except TypeError:
+#         print('There was a type error!')
+#     except ZeroDivisionError:
+#         print('There was a zero division error!')
+#     except Exception:
+#         print('There was some sort of error!')
+
+    
+# causeError()
+
+# Custome decorators
+def handleException(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except TypeError:
+            print('There was a type error!')
+        except ZeroDivisionError:
+            print('There was a zero division error!')
+        except Exception:
+            print('There was some sort of error!')
+    return wrapper
+
+@handleException
 def causeError():
-    try:
-        return 1/0
-    except Exception as e:
-        print(type(e))
-    finally:
-        print("This block always executed")
+    return 1/0
+
 causeError()

4c8aefd2a512184609fdb53a5180998c87cf1140
annamani <annamanik.vit@gmail.com>
2023-11-23 20:56:38 +0100

    classes

--- methods.py
+++ methods.py
@@ -1,4 +1,28 @@
+# class WordSet:
+#     def __init__(self):
+#         self.words = set()
+        
+#     def addText(self, text):
+#         text = WordSet.cleanText(text)
+#         for word in text.split():
+#             self.words.add(word)
+            
+#     def cleanText(text):
+#         # chaining functions
+#         text = text.replace('!', '').replace('.', '').replace(',', '').replace('\'', '')
+#         return text.lower()
+    
+        
+# wordSet = WordSet()
+
+# wordSet.addText('Hi, I\'m Ryan! Here is a sentence I want to add!')
+# wordSet.addText('Here is another sentence I want to add.')
+
+# print(wordSet.words)
+
+
 class WordSet:
+    replacePuncs=['!','.',',','\'']
     def __init__(self):
         self.words = set()
         
@@ -7,9 +31,11 @@ class WordSet:
         for word in text.split():
             self.words.add(word)
             
+    @staticmethod
     def cleanText(text):
         # chaining functions
-        text = text.replace('!', '').replace('.', '').replace(',', '').replace('\'', '')
+        for punc in WordSet.replacePuncs:
+            text = text.replace(punc, '')
         return text.lower()
     
         

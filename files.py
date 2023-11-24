14967aaefa5898091d1dcd3f437540bb9b991036
annamani <annamanik.vit@gmail.com>
2023-11-24 18:28:26 +0100

    files

---
+++ files.py
@@ -0,0 +1,19 @@
+# Reading a File
+# f = open('samplefile.txt', 'r')
+# print(f.readline())
+
+# Writing into a file
+# f=open('samplefile.txt','w')
+# print(f.write("Annamani\n"))
+
+# Appending files
+f = open('samplefile.txt', 'a')
+f.write('Kamma\n')
+f.write('Software Engineer\n')
+f.close()
+
+
+
+
+
+f.close()

156fe9016f109ff2320c6691468df2b2bc2b212c
annamani <annamanik.vit@gmail.com>
2023-11-29 18:37:27 +0100

    threads

--- thread.py
+++ thread.py
@@ -5,3 +5,11 @@ def longSquare(num):
     return num**2
 
 [longSquare(n) for n in range(0, 5)]
+t1 = threading.Thread(target=longSquare, args=(1,))
+t2 = threading.Thread(target=longSquare, args=(2,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
